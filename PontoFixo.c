3270c660ff9270daa145c4ccf7b377eb75df4eb7
Camila Martins <37345577+camila-martins@users.noreply.github.com>
2018-04-07 21:26:16 -0300

    Add files via upload

---
+++ PontoFixo.c
@@ -0,0 +1,37 @@
+//Tarefa 2 - Métodos Numéricos 2
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+FILE *fp;
+
+int SolucaoPontoFixo(double *x0, double prec)
+{
+	double x, dx, c=0;
+	fp=fopen("dados2.dat","w");
+	do
+	{
+		x=cos(*x0);
+		dx=fabs(x- *x0);
+		*x0=x;
+		fprintf(fp,"%g\n", *x0);
+		c=c+1;
+	}while(dx>prec);
+	fclose(fp);
+	return (c);
+
+}
+
+int main( int argc,char **argv)
+{
+	double x, x0, dx, prec;
+	
+	x0= atof(argv[1]);
+	prec=atof(argv[2]);
+	
+	printf("\n Raiz = %.10g em %d interacoes\n", x0 ,SolucaoPontoFixo(&x0, prec));
+	
+	return 0;
+
+}
+	
